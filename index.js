da5577f30c7747130dbb8adc24e8b012d5e468d7
christinacephus@pursuit.org <christinacephus@pursuit.org>
2022-10-17 22:01:03 -0400

    Team Lab of 5

--- index.js
+++ index.js
@@ -4,7 +4,10 @@
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function addNewStore(stores, store) {}
+function addNewStore(stores, store) {
+  stores.push(store)
+  return stores
+}
 
 /**
  * Removes a store object at the given position.
@@ -12,15 +15,37 @@ function addNewStore(stores, store) {}
  * @param {number} index - A number representing the index of the store to be removed from the array.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function removeStoreAtPosition(stores, index) {}
+function removeStoreAtPosition(stores, index) {
+  let newStores = [];
+  for (let i = index; i < stores.length; i++) {
+    stores[i] = stores[i+1]
+    }
+    stores.pop()
+
+  return stores
+}
 
 /**
  * Creates a duplicate of the `store` object. No references should be shared between the inputted `store` and the result.
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object} The duplicated store object. This should not be the same as the store that was inputted.
  */
-function duplicateStore(store) {}
-
+function duplicateStore(store) {
+  let newStores = {
+    name: store.name,
+    boardGames: [],
+    address: {
+      street: store.address.street,
+      city: store.address.city,
+      state: store.address.state,
+      zip: store.address.zip,
+  }
+ }
+ for(let i = 0; i < store.boardGames.length; i++){
+  newStores.boardGames[i] = store.boardGames[i]
+ }
+ return newStores
+}
 module.exports = {
   addNewStore,
   removeStoreAtPosition,
