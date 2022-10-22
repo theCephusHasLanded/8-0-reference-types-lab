9815f22899c95a2acc65968ee28e514962d90014
christinacephus@pursuit.org <christinacephus@pursuit.org>
2022-10-22 18:35:57 -0400

    Commenting Helps Me Learn*

---
+++ Assessment/src/06-loops.js
@@ -0,0 +1,247 @@
+/**
+ * rangeSum()
+ * ---------------------
+ * Takes in range (inclusive) and returns the sum of all numbers in that range.
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ *
+ * EXAMPLE:
+ *  rangeSum(3, 7);
+ *  //> 25
+ *  (3 + 4 + 5 + 6 + 7 = 25)
+ */
+function rangeSum(min, max) {
+  let sum = 0
+  for (let i = min; i <= max; i++){
+    sum += i
+  }
+  return sum
+}
+
+/**
+ * rangeOdd()
+ * ---------------------
+ * Takes in range (inclusive) and returns an array in decreasing order of only the odd numbers in that range.
+ * @param {number} min
+ * @param {number} max
+ * @returns {number[]}
+ *
+ * EXAMPLE:
+ *  rangeOdd(10, 15);
+ *  //> [ 15, 13, 11 ]
+ */
+function rangeOdd(min, max) {
+  let odd = [] // call your array what you want 
+  for (let i = max; i >= min; i--){ //initiate the loop so that you can start from the maximum and decrement to the min 
+    if (i % 2 !== 0) // if the index is divisible by 2 and not equal to zero then it is odd
+      odd.push(i) // now add the odd's in a push to the array
+  }
+  return odd // call your array.
+}
+
+/**
+ * rangeEveryOther()
+ * ---------------------
+ * Takes in range (inclusive) and returns an array in increasing order of only every other element. Start by including the first number.
+ * @param {number} min
+ * @param {number} max
+ * @returns {number[]}
+ *
+ * EXAMPLE:
+ *  rangeEveryOther(11, 18);
+ *  //> [ 11, 13, 15, 17 ]
+ */
+function rangeEveryOther(min, max) {
+  let everyOther = [] // declare a new array
+  for (let i = min; i <= max; i+= 2) { //start your loop at the minimum and increment so that it goes through the function and gives the "second number" rather than every number
+    everyOther.push(i) // now add the every other number in a push to the array.
+}
+return everyOther
+} // * += will tell loop what you want it to do* 
+
+/**
+ * containsWhileLoop()
+ * ---------------------
+ * Returns whether or not the `target` value exists in the array.
+ *
+ * NOTE: Must be written with a `while` loop.
+ *
+ * NOTE: Must NOT contain the `.includes()` method.
+ *
+ * @param {*[]} array
+ * @param {number|string} target
+ * @returns {boolean}
+ *
+ * EXAMPLE:
+ *  containsWhileLoop([ 5, 2, 4 ], 4);
+ *  //> true
+ *
+ *  containsWhileLoop([ "left", "up", "right" ], "down");
+ *  //> false
+ */
+function containsWhileLoop(array, target) {
+let i = 0 // same process like for loop but break it down logic is before the incrementation
+while(i < array.length){ //now iterate the loop through the length of array
+    if (array[i] === target)
+      return true // return true if you DO find "target"
+    i++ // increment that thang through the loop!
+  }
+  return false // only return this if you DON'T find "target"
+}
+
+/**
+ * containsForLoop()
+ * ---------------------
+ * Returns whether or not the `target` value exists in the array.
+ *
+ * NOTE: Must be written with a `for` loop.
+ *
+ * NOTE: Must NOT contain the `.includes()` method.
+ *
+ * @param {*[]} array
+ * @param {number|string} target
+ * @returns {boolean}
+ *
+ * EXAMPLE:
+ *  containsForLoop([ 5, 2, 4 ], 4);
+ *  //> true
+ *
+ *  containsForLoop([ "left", "up", "right" ], "down");
+ *  //> false
+ */
+function containsForLoop(array, target) {
+  for (let i = 0; i < array.length; i++){
+      if (array[i] === target){
+        return true
+      }// end of condition
+  }return false//end of for loop
+} //end of function
+
+/**
+ * targetCount()
+ * ---------------------
+ * Returns how many times the `target` occurs in the array.
+ * @param {*[]} array
+ * @param {number|string} target
+ * @returns {number}
+ *
+ * EXAMPLE:
+ *  targetCount([ 10, 20, 10, 20, 30 ], 10);
+ *  //> 2
+ */
+function targetCount(array, target) {
+  let count = 0
+for (let i = 0; i < array.length; i++){ // let this index be initilaized, then 
+    if (array[i] === target){
+      count++ // count must increment with the condition 
+      //when a function returns it STOPS
+    }
+} return count // return the target count
+}
+
+/**
+ * firstIndexFound()
+ * ---------------------
+ * Returns the first index that the `target` is found at in the array. If the `target` is not found, it should return `-1`.
+ *
+ * NOTE: You may NOT use the `.indexOf()` method.
+ *
+ * @param {*[]} array
+ * @param {number|string} target
+ * @returns {number}
+ *
+ * EXAMPLE:
+ *  firstIndexFound([ "left", "right", "left" ], "left");
+ *  //> 0
+ *
+ *  firstIndexFound([ "left", "right", "left" ], "up");
+ *  //> -1
+ */
+function firstIndexFound(array, target) {
+  for (let i = 0; i < array.length; i++){//heres the loop iteration
+    if (array[i] === target){ //heres your conditional
+      return i // if you find the index give it up!!!
+    }
+  } return -1 // if not then return a string of `-1`
+  }//end of function
+  
+
+/**
+ * lastIndexFound()
+ * ---------------------
+ * Returns the last index that the `target` is found at in the array. If the `target` is not found, it should return `-1`.
+ *
+ * NOTE: You may NOT use the `.indexOf()` method.
+ *
+ * @param {*[]} array
+ * @param {number|string} target
+ * @returns {number}
+ *
+ * EXAMPLE:
+ *  lastIndexFound([ "left", "right", "left" ], "left");
+ *  //> 2
+ *
+ *  lastIndexFound([ "left", "right", "left" ], "up");
+ *  //> -1
+ */
+function lastIndexFound(array, target) {
+  for (let i = array.length; i >= 0; i--){
+    if(array[i] === target){
+      return i // GIVE it UP!
+    }
+  } return -1
+}
+
+/**
+ * timesIndex()
+ * ---------------------
+ * Returns a new array where each element has been multiplied by its index.
+ * @param {number[]} elements
+ * @returns {number[]}
+ *
+ * EXAMPLE:
+ *  timesIndex([ 7, 10, 11 ]);
+ *  //> [ 7 * 0, 10 * 1, 11 * 2 ]
+ *  //> [ 0, 10, 22 ]
+ */
+function timesIndex(array) {
+  for (let i = 0; i < array.length; i++){
+    array[i] *= i  //multiplying the array.length index by it's character placement in array
+  }return array //outside of loop
+  }
+
+/**
+ * cumulativeSum()
+ * ---------------------
+ * Returns a new array where each element is the cumulative sum in the array, up until that point.
+ * @param {number[]} array
+ * @returns {number[]}
+ *
+ * EXAMPLE:
+ *  cumulativeSum([ 5, 2, 9 ]);
+ *  //> [ 5, 5 + 2, 5 + 2 + 9 ]
+ *  //> [ 5, 7, 16 ]
+ */
+function cumulativeSum(array) {
+  let boo = []
+  let sum = 0
+  for (let i = 0; i < array.length; i++){
+    sum +=  array[i]
+    boo.push(sum)
+  } return boo
+}
+
+// Do not change anything below this line.
+module.exports = {
+  rangeSum,
+  rangeOdd,
+  rangeEveryOther,
+  containsWhileLoop,
+  containsForLoop,
+  targetCount,
+  firstIndexFound,
+  lastIndexFound,
+  timesIndex,
+  cumulativeSum,
+};
